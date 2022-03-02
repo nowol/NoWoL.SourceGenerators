2e6c19cdc014233a7b19eeb5ed58cc39e0504269
NoWoL <nowolos@gmail.com>
2022-03-01 22:08:13 -0500

    Improve code coverage take 4

--- src/NoWoL.SourceGenerators/ExceptionGenerator.cs
+++ src/NoWoL.SourceGenerators/ExceptionGenerator.cs
@@ -165,7 +165,7 @@ namespace NoWoL.SourceGenerators
 
         private static AttributeData GetExceptionGeneratorAttribute(ISymbol targetType, INamedTypeSymbol excepGeneratorAttr)
         {
-            return targetType.GetAttributes().FirstOrDefault(x => x.AttributeClass?.Equals(excepGeneratorAttr, SymbolEqualityComparer.Default) ?? false);
+            return targetType.GetAttributes().FirstOrDefault(x => x.AttributeClass!.Equals(excepGeneratorAttr, SymbolEqualityComparer.Default));
         }
 
         private void GenerateException(IndentedStringBuilder sb, ISymbol targetType, ClassDeclarationSyntax target, AttributeData exceptionGeneratorAttribute, string ns)
